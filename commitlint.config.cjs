4bc8279fb63ece5035d0c113aebd0937f2dd35a6
Thailo Eduardo <thailoeduardo@gmail.com>
2025-03-13 18:26:43 -0300

    feat: automate changelogs
    Added Commitlint and Husky to automate changelogs with github actions

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -5,19 +5,19 @@ module.exports = {
         2,
         'always',
         [
-          'feat',   // Funcionalidade nova
+          'feat',   // Nova funcionalidade
           'fix',    // Correção de bug
           'docs',   // Documentação
-          'style',  // Apenas formatação (sem mudança no código)
+          'style',  // Formatação (sem mudança no código)
           'refactor', // Refatoração (sem mudança na lógica)
           'perf',   // Melhorias de desempenho
-          'test',   // Adiciona/atualiza testes
+          'test',   // Testes adicionados/alterados
           'chore',  // Alterações no build ou ferramentas
           'release' // Lançamento de nova versão
         ]
       ],
       'type-empty': [2, 'never'], // Tipo não pode estar vazio
       'subject-empty': [2, 'never'], // Assunto não pode estar vazio
-      'subject-case': [0], // Permite qualquer capitalização no assunto
+      'subject-case': [0], // Permite qualquer formatação no assunto
     }
   };
