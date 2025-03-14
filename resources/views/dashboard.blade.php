bff34ed98c3e8706f81d01fb9986937d9e6bdea6
Thailo Eduardo <thailoeduardo@gmail.com>
2025-03-13 21:24:25 -0300

    feat: teste

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -44,4 +44,56 @@
             </div>
         </div>
     </div> --}}
+<div class="flex min-h-[6rem] max-w-4xl min-w-[18rem] flex-wrap items-center justify-center gap-2 overflow-x-hidden mx-auto ">
+    <div class="w-full max-w-lg">
+        <ul class="list bg-base-100 rounded-box shadow-md">
+
+            {{-- <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Most played songs this week</li> --}}
+
+            <li class="list-row">
+            <div class="text-4xl font-thin opacity-30 tabular-nums">01</div>
+            <div>
+                {{-- <img class="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/1@94.webp"/> --}}
+                <div class="avatar">
+                    <div class="mask mask-hexagon w-8">
+                      <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
+                  </div>
+            </div>
+            <div class="list-col-grow">
+                <div>Dio Lupa</div>
+                <div class="text-xs uppercase font-semibold opacity-60">Remaining Reason</div>
+            </div>
+            <button class="btn btn-square btn-ghost">
+                <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><path d="M6 3L20 12 6 21 6 3z"></path></g></svg>
+            </button>
+            </li>
+
+            <li class="list-row">
+            <div class="text-4xl font-thin opacity-30 tabular-nums">02</div>
+            <div><img class="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/4@94.webp"/></div>
+            <div class="list-col-grow">
+                <div>Ellie Beilish</div>
+                <div class="text-xs uppercase font-semibold opacity-60">Bears of a fever</div>
+            </div>
+            <button class="btn btn-square btn-ghost">
+                <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><path d="M6 3L20 12 6 21 6 3z"></path></g></svg>
+            </button>
+            </li>
+
+            <li class="list-row">
+            <div class="text-4xl font-thin opacity-30 tabular-nums">03</div>
+            <div><img class="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/3@94.webp"/></div>
+            <div class="list-col-grow">
+                <div>Sabrino Gardener</div>
+                <div class="text-xs uppercase font-semibold opacity-60">Cappuccino</div>
+            </div>
+            <button class="btn btn-square btn-ghost">
+                <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><path d="M6 3L20 12 6 21 6 3z"></path></g></svg>
+            </button>
+            </li>
+
+        </ul>
+    </div>
+</div>
 </x-app-layout>
