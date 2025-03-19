c8bd99382e65807549086f4384a20962b73360f8
Thailo Eduardo <thailoeduardo@gmail.com>
2025-03-19 20:56:43 -0300

    feat(controller): adicionado controller Dashboard
    Adicionado e ajustado o controller para o dashborad

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Room;
+use App\Models\RoomPoints;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    // show rooms
+    public function index()
+    {
+        $rooms = Room::orderBy('created_at', 'desc')->get();
+        $totalPoints = [];
+
+        foreach ($rooms as $room) {
+            $totalPoints[$room->id] = [
+                'room_id' => $room->id,
+                'points' => RoomPoints::where('room_id', $room->id)
+                    ->with('user')
+                    ->selectRaw('player_id, SUM(points) as total_points')
+                    ->groupBy('player_id')
+                    ->get()
+                    ->keyBy('player_id'),
+            ];
+
+        }
+
+        return view('dashboard', ['totalPoints' => $totalPoints]);
+    }
+}
