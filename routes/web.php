5ae0f5287b5cb0857fb4f4310872a9ab80a3a607
Thailo Eduardo <thailoeduardo@gmail.com>
2025-03-27 07:00:15 -0300

    feat(routes): ajustes no codigo referente as rotas
    Comentario sobre cada rota e ordenação das rotas

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
-use Illuminate\Http\Request;
-use App\Models\PlayerRoom;
-use App\Models\Room;
-use App\Models\RoomPoints;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PlayersController;
@@ -17,7 +12,12 @@ Route::get('/', [HomeController::class, 'show'])
     ->middleware('guest')
     ->name('home');
 
-// Lista as ultiams salsa disponiveis
+// Dashboard
+Route::get('dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+// Listar as ultimas salas disponiveis
 Route::get('/rooms', [RoomsController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('rooms');
@@ -32,56 +32,42 @@ Route::get('/room/{id}', [RoomsController::class, 'show']
     )->middleware(['auth', 'verified'])
     ->name('room');
 
-// Add player to room
+// Adicionar jogador na sala
 Route::post('/room/{id}', [RoomsController::class, 'addPlayer'])
     ->middleware(['auth', 'verified'])
     ->name('room');
 
-// Add point to player
+// Adicionar ponto ao jogador
 Route::post('/room/{id}/point', [RoomsController::class, 'addPoint'])
     ->middleware(['auth', 'verified'])
     ->name('room');
 
-// Route::delete('/room/{id}', function ($id) {
-//     $room = Room::findOrFail($id);
-//     $room->delete();
-//     return redirect('/rooms');
-// })->middleware(['auth', 'verified']);
-
+// Apagar sala
 Route::delete('/room/{id}', [RoomsController::class, 'destroy'])
     ->middleware(['auth', 'verified'])
     ->name('room.destroy');
 
-// [RoomsController::class, 'delete']
-//     )->middleware(['auth', 'verified'])
-//     ->name('room');
-
-// Show player points
+// Exibir pontos do jogador
 Route::get('points/{id}/room/{room_id}', [PointsController::class, 'show'])
     ->middleware(['auth', 'verified'])
     ->name('points');
 
-// Update player points
+// Atualziar pontos do jogador
 Route::patch('points/{id}/room/{room_id}', [PointsController::class, 'update'])
     ->middleware(['auth', 'verified'])
     ->name('points');
 
-// Delete player points
+// Apagar pontos do jogador
 Route::delete('/points/{point_id}/room/{room_id}', [PointsController::class, 'destroy'])
     ->middleware(['auth', 'verified'])
     ->name('points');
 
-// Show rooms dashboard
-Route::get('dashboard', [DashboardController::class, 'index'])
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-// Show profile
+// Exibir os dados do jogador
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-// Show all players
+// Exibir todos os jogadores
 Route::get('players', [PlayersController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('players');
